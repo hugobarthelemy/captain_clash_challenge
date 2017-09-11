34abb3805573bd493533a19702e4bc791012fb33
hugo barthelemy <barthelemyhugo@gmail.com>
2017-09-11 15:19:02 +0200

    add perso controller : new / create / index / destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def index         # GET /persos
+    @persos = Perso.all
+  end
+
+  def new           # GET /persos/new
+    @perso = Perso.new
+  end
+
+  def create        # POST /persos
+    @perso = Perso.new(perso_params)
+    @perso.save
+    redirect_to persos_path
+  end
+
+  def destroy       # DELETE /persos/:id
+    @perso = Perso.find(params[:id])
+    @perso.destroy
+  end
+
+  private
+
+  def perso_params
+    params.require(:perso).permit(:name, :points_of_life, :attack_points, :picture_url)
+  end
 end
---
+++ app/controllers/persos_controller.rb
@@ -0,0 +1,2 @@
+class PersosController < ApplicationController
+end
